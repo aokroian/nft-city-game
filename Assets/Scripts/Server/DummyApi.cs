a58393d3deabd2866e5ecd6e70c83ec181ea275e
GastarTestProjects <53343040+GastarTestProjects@users.noreply.github.com>
2022-06-03 12:48:53 +0300

    Add server api provider

--- Assets/Scripts/Server/DummyApi.cs
+++ Assets/Scripts/Server/DummyApi.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ServerApi : AbstractServerApi
+public class DummyApi : AbstractServerApi
 {
-    
 }
---
+++ Assets/Scripts/Server/ServerApiProvider.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ServerApiProvider : MonoBehaviour
+{
+    public AbstractServerApi concreteApi;
+}
