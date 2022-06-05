94643a2f779fed4061a6d5ae3edd58891aeb4da9
GodDoesPlayDice <alik_okroyan@mail.ru>
2022-06-05 08:24:57 +0300

    simple tweening + deleted rider flow again lol

--- Assets/Scripts/UI/AnimatedUI.cs
+++ Assets/Scripts/UI/AnimatedUI.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using DG.Tweening;
 
@@ -5,6 +6,32 @@ namespace UI
 {
     public class AnimatedUI : MonoBehaviour
     {
-        
+        public bool expandOnEnable = true;
+        [Range(0.01f, 1f)]public float expandTime = 0.5f;
+        public float punchMultiplier = 1.1f;
+        public float punchDuration = 0.1f;
+        public int punchVibrato = 10;
+        public float punchElasticity = 1f;
+
+        [SerializeField] private RectTransform rectTransform;
+
+        private void Awake()
+        {
+            rectTransform ??= GetComponent<RectTransform>();
+        }
+
+        private void OnEnable()
+        {
+            if (expandOnEnable)
+            {
+                rectTransform.localScale = Vector3.zero;
+                rectTransform.DOScale(Vector3.one, expandTime).OnComplete(() =>
+                {
+                    rectTransform.DOPunchScale(Vector3.one * punchMultiplier, punchDuration, punchVibrato, punchElasticity);
+                });
+
+            }
+            
+        }
     }
 }
